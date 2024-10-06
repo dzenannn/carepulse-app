a643acf4bb228e77bf9472c7394bfb58c609d8a2
dzenann <dzenan.dev@gmail.com>
2024-10-06 23:43:53 +0200

    Custom Form Field

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FieldTypes } from "./forms/PatientForm";
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FieldTypes;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const RenderInput = ({ field, props }: { field: any; props: CustomProps }) => {
+  return <Input type="text" placeholder="Enter your name" />;
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FieldTypes.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+          <RenderInput field={field} props={props} />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import CustomFormField from "../CustomFormField";
+
+export enum FieldTypes {
+  INPUT = "input",
+  SELECT = "select",
+  DATE = "date",
+  TIME = "time",
+  CHECKBOX = "checkbox",
+  RADIO = "radio",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  SKELETON = "skeleton",
+}
+
+const formSchema = z.object({
+  username: z.string().min(5, {
+    message: "Username must be at least 5 characters.",
+  }),
+});
 
 const PatientForm = () => {
-  return <div>patient Form</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there! 🙃</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField
+          fieldType={FieldTypes.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
